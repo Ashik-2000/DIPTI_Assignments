b8bfecf3726229c46dd75ad1fa0560a9dc5e3786
Ashikul Islam <ashikulislamfb@gmail.com>
2024-09-27 22:20:17 +0600

    HW

--- practice.py
+++ practice.py
@@ -1,2 +1,17 @@
-a = 10
-print(a)
+class Students:
+    Id=1
+    Student_Name="R"
+    Cell_no="5464654"
+    Email="r@gmail, com"
+    Class_Status="Offline"
+    Attendance="100"
+
+
+print("ID Student_Name cell email status attendence" )
+
+print(Students. Id, 
+    Students .Student_Name, 
+    Students.Cell_no, 
+    Students. Email, 
+    Students. Class_Status, 
+    Students.Attendance)
